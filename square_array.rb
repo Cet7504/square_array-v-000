02ed2f6f406e6eafde56d087e100dfb6835d37eb
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2020-11-12 02:14:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-  new_array = []
-  array.each{|square| new_array.push(square**2)}
-  return new_array
+  squared = []
+  array.each {|element| squared << element ** 2}
+  squared
 end
